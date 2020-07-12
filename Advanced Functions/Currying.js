1635383383812b77588a04407f9a9fdebc552066
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-12 20:30:40 +0200

    Create Currying.js

---
+++ Advanced Functions/Currying.js
@@ -0,0 +1,17 @@
+/*
+Currying is a technique for function decomposition
+*/
+
+function sum3(a) {
+
+    return (b) => {
+
+        return (c) => {
+
+            return a + b + c;
+
+        }
+    }
+}
+
+console.log(sum3(5)(6)(8));
