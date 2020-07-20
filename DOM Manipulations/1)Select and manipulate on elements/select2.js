368a44ee44150ea2003495b600828f25491b419d
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-20 20:14:25 +0200

    Create select2.js

---
+++ DOM Manipulations/1)Select and manipulate on elements/select2.js
@@ -0,0 +1,8 @@
+const list = document.querySelector('.list');
+console.log(list);
+
+const lis = document.querySelectorAll('ul li');
+console.log(lis);
+
+const listItems = document.querySelectorAll('.list-item');
+console.log(listItems);
