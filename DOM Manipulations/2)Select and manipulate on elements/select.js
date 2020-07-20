68423becc0aa0f567ce472107983e8c6762eb48f
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-20 20:10:24 +0200

    Create select.js

---
+++ DOM Manipulations/2)Select and manipulate on elements/select.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById('heading');
+console.log(heading);
+
+const list = document.getElementById('list');
+console.log(list.textContent);
+
+console.log(list.innerHTML);
+
+list.textContent = '<h1>Welcome! Please, sit.</h1>';
