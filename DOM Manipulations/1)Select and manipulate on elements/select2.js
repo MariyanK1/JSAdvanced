ba426320ff4a9e05d1bcd7d8a9b4a8ebe9f6377e
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-25 20:45:01 +0200

    Delete select2.js

--- DOM Manipulations/1)Select and manipulate on elements/select2.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelector('.list');
-console.log(list);
-
-const lis = document.querySelectorAll('ul li');
-console.log(lis);
-
-const listItems = document.querySelectorAll('.list-item');
-console.log(listItems);
