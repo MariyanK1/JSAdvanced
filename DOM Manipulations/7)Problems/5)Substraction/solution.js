c827f89dcbdf09a4410f290b6e472ce349bdeb1b
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-07-31 16:29:24 +0200

    Update solution.js

--- DOM Manipulations/7)Problems/5)Substraction/solution.js
+++ DOM Manipulations/7)Problems/5)Substraction/solution.js
@@ -1,3 +1,19 @@
+/*
+Implement the above to provide the following functionality:
+ Your function should take the values of "Number" and secondNumber, convert them to
+numbers, subtract the second number from the first one and then append the result to the <div> with id="result".
+ Your function should be able to work with any 2 numbers in the inputs, not only the ones given in
+the example.
+
+*/
+
 function subtract() {
-    console.log('TODO:...');
+    const $firstInputElement = document.getElementById('firstNumber');
+    const $secondInputElement = document.getElementById('secondNumber');
+    const $divElement = document.getElementById('result');
+
+    const result = +$firstInputElement.value - +$secondInputElement.value;
+
+    $divElement.textContent = result;
+
 }
