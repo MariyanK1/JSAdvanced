ab19554d65dd39d35f26368d44078b9d4d64df7c
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-18 21:45:07 +0200

    Update solution.js

--- DOM Manipulations/7)Problems/7)Accordion/solution.js
+++ DOM Manipulations/7)Problems/7)Accordion/solution.js
@@ -1,1 +1,21 @@
-//TODO
+/*
+An html file is given and your task is to show more/less information by clicking a [ADD] button (it is not an
+actual button, but a span that has an onclick event attached to it).
+When [More] button is clicked, it reveals the content of a hidden div and changes the text of the button to [Less].
+When the same link is clicked again (now reading Less), hide the div and change the text of the link to More.
+Link action should be toggleable (you should be able to click the button infinite amount of times).
+*/
+
+
+function toggle() {
+    const $divs = document.querySelectorAll("div > div");
+    const $button = $divs[0].querySelector('span.button');
+
+    if ($button.textContent === 'More') {
+        $button.textContent = 'Less';
+        $divs[1].style.display = "block";
+    } else {
+        $button.textContent = 'More';
+        $divs[1].style.display = "none";
+    }
+}
