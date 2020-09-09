8d6b39743d0a86cf1a28f5ce277e82e30797eed4
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-09-09 10:36:05 +0200

    Update closure.js

--- Advanced Functions/closure.js
+++ Advanced Functions/closure.js
@@ -1,15 +1,26 @@
 /*
-A state is preserved in the outer function (closure)
+One of the most important features in Javascript.
+The context of an inner function includes the scope of the outer function.
+An inner function enjoys that context even after the parent function have returned.
+A state is preserved in the outer function (closure).
 */
 
 const f = (function () {
 
-let counter = 0;
+    let counter = 0;
 
-return function () {
+    return function () {
 
-console.log(++counter);
+        console.log(++counter);
 
-}
+    }
 
 })();
+
+f(); //1
+f(); //2
+f(); //3
+f(); //4
+f(); //5
+f(); //6
+
