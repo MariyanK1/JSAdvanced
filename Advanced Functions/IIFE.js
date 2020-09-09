4e6cdfa871ccd99bc3f0c5ec71294ba4ad000178
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-09-09 10:29:57 +0200

    Create IIFE.js

---
+++ Advanced Functions/IIFE.js
@@ -0,0 +1,12 @@
+/*
+Define anonymous function expression
+
+Invoke it immediately after declaration
+*/
+
+let result = (function () {
+    let name = 'Mariyan';
+    return name;
+})();
+
+console.log(result);
