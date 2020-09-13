44c3a35f2682b034e9373c11847fbf00dcc59a80
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-09-13 11:17:48 +0200

    Update solution.js

--- DOM Manipulations/7)Problems/12)Order the Names/solution.js
+++ DOM Manipulations/7)Problems/12)Order the Names/solution.js
@@ -1,3 +1,15 @@
+/*
+Write a function that orders names alphabetically.
+ 
+You will receive a name of a student as an input. When the "ADD" button is clicked, you should
+add the given student name in the SoftUni Database, while keeping the alphabetial order.
+For instance, if we register David, his name should appear in the D column.
+
+If you receive more than one name with the same starting letter, you should join all names by a 
+comma and a space (", ").
+
+*/
+
 function solve() {
     let addButton = document.getElementsByTagName("button")[0];
     let inputTextElement = document.getElementsByTagName("input")[0];
