b2dd8d15c0b28776dc0274ef0076ed2c4efb9677
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-10-11 20:45:28 +0200

    Update bank.js

--- Classes/bank.js
+++ Classes/bank.js
@@ -2,94 +2,66 @@ class Bank {
     constructor(bankName) {
         this._bankName = bankName;
         this.allCustomers = [];
+        this.transactions = [];
     }
 
     newCustomer(customer) {
-        this.allCustomers.find((c) => {
-            if (c.personalId === customer.personalId) {
-                throw new Error(
-                    `${customer.firstName} ${customer.lastName} is already our customer!`
-                );
-            }
+        const findCustomer = this.allCustomers.find((x) => {
+            return x.firstName === customer.firstName || x.lastName === customer.lastName
         });
-        this.allCustomers.push(customer);
-        return customer;
+
+        if (findCustomer) {
+            throw Error(`${findCustomer.firstName} ${findCustomer.lastName} is already our customer!`)
+        } else {
+            this.allCustomers.push(customer);
+            return customer;
+        }
     }
 
     depositMoney(personalId, amount) {
-        let person = this.allCustomers.find((c) => {
-            return c.personalId === personalId;
-        });
-        if (!person) {
-            throw new Error('We have no customer with this ID!');
-        } else {
-            if (isNaN(person.totalMoney)) {
-                person.totalMoney = amount;
-            } else {
-                person.totalMoney += amount;
-            }
-            if (!Array.isArray(person.transactionInfos)) {
-                person.transactionInfos = [];
-            }
-            let message = `${person.transactionInfos.length + 1}. ${person.firstName
-                } ${person.lastName} made deposit of ${amount}$!`;
-            person.transactionInfos.push(message);
-            return `${person.totalMoney}$`;
+        const findCustomer = this.allCustomers.find((x) => { return x.personalId === personalId });
+
+        if (!findCustomer) {
+            throw Error('We have no customer with this ID!')
         }
+        if (isNaN(findCustomer['totalMoney'])) {
+            findCustomer['totalMoney'] = amount;
+        } else {
+            findCustomer['totalMoney'] += amount;
+        }
+        this.transactions.push(`${findCustomer.firstName} ${findCustomer.lastName} made deposit of ${amount}$!`)
+        return `${findCustomer['totalMoney']}$`
     }
 
     withdrawMoney(personalId, amount) {
-        let person = this.allCustomers.find((c) => {
-            return c.personalId === personalId;
-        });
-        if (!person) {
-            throw new Error('We have no customer with this ID!');
-        } else {
-            if (isNaN(person.totalMoney)) {
-                person.totalMoney = amount;
-            }
-            if (!Array.isArray(person.transactionInfos)) {
-                person.transactionInfos = [];
-            }
-            if (person.totalMoney < amount) {
-                throw new Error(`${person.firstName} ${person.lastName} does not have enough money to withdraw that amount!`);
-            } else {
-                person.totalMoney -= amount;
+        const findCustomer = this.allCustomers.find((x) => { return x.personalId === personalId });
 
-                let message = `${person.transactionInfos.length + 1}. ${person.firstName
-                    } ${person.lastName} withdrew ${amount}$!`;
-
-                person.transactionInfos.push(message);
-
-                return `${person.totalMoney}$`;
-            }
+        if (!findCustomer) {
+            throw Error('We have no customer with this ID!');
         }
+
+        if (findCustomer.totalMoney <= 0) {
+            throw Error(`${findCustomer.firstName} ${findCustomer.lastName} does not have enough money to withdraw that amount!`)
+        }
+
+        findCustomer.totalMoney -= amount;
+        this.transactions.push(`${findCustomer.firstName} ${findCustomer.lastName} withdrew ${amount}$!`);
+        return `${findCustomer.totalMoney}$`
     }
 
     customerInfo(personalId) {
-        let person = this.allCustomers.find((c) => {
-            return c.personalId === personalId;
-        });
-        if (!person) {
-            throw new Error('We have no customer with this ID!');
-        } else {
-            let infoString = '';
-            infoString += `Bank name: ${this._bankName}\n`;
-            infoString += `Customer name: ${person.firstName} ${person.lastName}\n`;
-            infoString += `Customer ID: ${person.personalId}\n`;
-            infoString += `Total Money: ${person.totalMoney}$\n`;
-            infoString += `Transactions:\n`;
-            for (let index = person.transactionInfos.length - 1; index >= 0; index--) {
-                infoString += person.transactionInfos[index];
-                if (index !== 0) {
-                    infoString += `\n`;
-                }
-            }
-            return infoString;
-        }
+        const info = this.allCustomers.find((x) => { return x.personalId === personalId });
+        let transaction = this.transactions.filter((x) => { return x.includes(info.firstName) });
+        let reversed = transaction.reverse();
+
+        let result = `Bank name: ${this._bankName}\nCustomer name: ${info.firstName} ${info.lastName}\nCustomer ID: ${info.personalId}\nTotal Money:                            ${info.totalMoney}$\nTransactions:`;
+        let counter = reversed.length;
+
+        reversed.forEach(x => result += '\n' + `${counter--}. ` + x);
+
+        return result.trim();
     }
 }
-
 const bank = new Bank('Mariyan');
 console.log(bank.newCustomer({ firstName: 'Mariyan', lastName: 'Maksimov', personalId: 950527 }));
 console.log(bank.depositMoney(950527, 34242));
