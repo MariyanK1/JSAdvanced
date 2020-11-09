f711821730aae1d80771bc8e3fde703631d434bb
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-11-09 18:45:35 +0100

    Update stringExtension.js

--- Object Composition/stringExtension.js
+++ Object Composition/stringExtension.js
@@ -63,18 +63,18 @@
 /*
 Extend the build-in String object with additional functionality. Implement the following functions:
 
-⦁	ensureStart(str) - append str to the beginning of a string, only if it’s not already present
+⦁ensureStart(str) - append str to the beginning of a string, only if it’s not already present
 
-⦁	ensureEnd(str) - append str to the end of a string, only if it’s not already present
+⦁ensureEnd(str) - append str to the end of a string, only if it’s not already present
 
-⦁	isEmpty() - return true if the string is empty, false otherwise
+⦁isEmpty() - return true if the string is empty, false otherwise
 
-⦁	truncate(n) - truncates the string to n characters by removing words and appends an ellipsis (three periods) to the end.
+⦁truncate(n) - truncates the string to n characters by removing words and appends an ellipsis (three periods) to the end.
 If a string is less than n characters long, return the same string.
 If it is longer, split the string where a space occurs and append an ellipsis to it so that the total length is less than or equal to n.
 If no space occurs anywhere in the string, return n - 3 characters and an ellipsis. If n is less than 4, return n amount of periods.
 
-⦁	format(string, …params) - static method to replace placeholders with parameters. A placeholder is a number surrounded by curly braces.
+⦁format(string, …params) - static method to replace placeholders with parameters. A placeholder is a number surrounded by curly braces.
 If parameter index cannot be found for a certain placeholder, do not modify it. Note static methods are attached to the String object instead of it’s prototype.
 See the examples for more info.
 Note strings are immutable, so your functions will return new strings as a result.
